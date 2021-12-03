92959f28cd0cd78e23a25bc814df07e10d595718
Mert Baskaya <mertb1998@gmail.com>
2021-12-03 14:31:26 +0300

    Added CustomerRepository

---
+++ src/main/java/mertbsk/springbootecommerce/dao/CustomerRepository.java
@@ -0,0 +1,7 @@
+package mertbsk.springbootecommerce.dao;
+
+import mertbsk.springbootecommerce.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
