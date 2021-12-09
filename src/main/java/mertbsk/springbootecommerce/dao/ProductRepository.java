eddccafec2d4058cfc4e60dccaf4e2d1c7d424fe
Mert <mertb1998@gmail.com>
2021-12-09 16:53:43 +0100

    Moved Cross Origin functionality to application.properties

--- src/main/java/mertbsk/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/mertbsk/springbootecommerce/dao/ProductRepository.java
@@ -4,7 +4,6 @@ import mertbsk.springbootecommerce.entities.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
 // CORS Politikasını aşmak için yazılır, CORS Politikası tarayıcılarda çalışan JavaScript uygulamaları için geçerlidir
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 // Aynı kökenli uygulamalardaki ayrım {{ http protokolü + host adı + port }} şeklindedir ve bunların hepsi aynı olmalıdır
 // {{ http://localhost:4200 }} != {{ http://localhost:8080 }}
 // @CrossOrigin yazıp bırakırsak API'yi aleni (herkese açık) yaparız
-@CrossOrigin("http://localhost:4200")
+// 09.12.2021 Düzenleme: Cross Origin düzenlemeleri application.properties dosyasına taşındı
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	// http://localhost:8080/api/products/search/findByCategoryId?id=1
