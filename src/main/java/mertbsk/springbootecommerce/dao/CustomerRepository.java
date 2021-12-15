8333190dc9ced5557e89ad9f5620ede2feb3dd87
Mert <mertb1998@gmail.com>
2021-12-15 16:06:27 +0100

    Update to CustomerRepository
    -- added findByEmail method

--- src/main/java/mertbsk/springbootecommerce/dao/CustomerRepository.java
+++ src/main/java/mertbsk/springbootecommerce/dao/CustomerRepository.java
@@ -4,4 +4,8 @@ import mertbsk.springbootecommerce.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	
+	// Veritabanında aranan bulunamazsa `null` döndürür
+	Customer findByEmail(String email);
+	
 }
